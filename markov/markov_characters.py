1dd791d471e3b7f82ee6e198e8b14e14d94c7d7a
Evan Bretl <evanbretl4@gmail.com>
2016-09-28 23:06:18 -0400

    added my old n-character generator program

---
+++ markov/markov_characters.py
@@ -0,0 +1,75 @@
+import random
+import os
+from glob import glob
+
+#user-controlled parameters
+ngram_lengths = (5,11,15)
+output_length = 1000
+sample_path = 'corpus/war_and_peace.txt'
+
+#initialize other variables
+ngram_lengths = tuple(sorted(ngram_lengths, reverse=True))
+ngram_dict = {}
+corpus = ''
+
+#read from sample
+for fname in glob(sample_path):
+    print('source', fname)
+    with open(fname, 'r') as txtfile:
+        gen = (c for line in txtfile 
+                 for c in line.replace('\n',' ')
+                 if c not in '.,?!@#$%^&*()\'":;<>/\\|[}{]'
+              )
+        for char in gen:
+            corpus += char
+        corpus = corpus.lower()
+
+#crawl over body of text for n-grams
+for n in ngram_lengths:
+    ngram_dict[n] = {}
+    print('learning %d-grams' % n)
+    for i in range(len(corpus)-n):
+        prev = corpus[i: i+n]
+        if prev in ngram_dict[n].keys():
+            ngram_dict[n][prev].append(corpus[i+n])
+        else:
+            ngram_dict[n][prev] = [corpus[i+n]]
+
+for n, subdict in ngram_dict.items():
+    #print(n, len(subdict))
+    avg = sum(len(item) for item in subdict.values()) / len(subdict)
+    print(n, 'has avg of', avg, 'entries per prev key')
+
+
+print('generating', output_length, 'characters of text')
+
+#initialize text to a random ngram from the longest class
+text = random.choice(tuple(ngram_dict[ngram_lengths[0]].keys()))
+
+#generate random text
+n_defaults = 0
+counts = {}
+for n in ngram_lengths:
+    counts[n] = 0
+
+for _ in range(output_length):
+    n_idx = random.randrange(len(ngram_lengths))
+    n_ideal = ngram_lengths[n_idx]
+    for n in ngram_lengths[n_idx:]:
+        prev = text[-n:]
+        if prev in ngram_dict[n].keys():
+            text += random.choice(ngram_dict[n][prev])
+            if not n == n_ideal:
+                #print('ideal is', n_ideal, 'but used', n)
+                n_defaults += 1
+            counts[n] += 1
+            break;
+
+print('length of ngram decreased', n_defaults, 'times')
+for n in ngram_lengths:
+    print('n=%d used %d times' % (n, counts[n])) 
+
+#with open('markov_output.txt', 'w') as output_file:
+#    output_file.write(text)
+
+print(text)

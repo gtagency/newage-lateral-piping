7aa09e3bd662c14a055672c459410ce0b3fc9fbb
valentinewilson <valentinewilson@lawn-128-61-41-232.lawn.gatech.edu>
2016-10-18 19:36:47 -0400

    - analogies

--- NLP/Word2Vec/test.py
+++ NLP/Word2Vec/test.py
@@ -3,4 +3,6 @@ from gensim.models import Word2Vec
 from gensim import corpora, models, similarities
 from nltk.corpus import brown
 b = Word2Vec(brown.sents())
-print(b.most_similar_cosmul('person', topn=5))
+print(b.most_similar_cosmul(positive=['person','baby'],negative = ["dog"], topn=5))
+#COCA corpus
+#save a mmodel

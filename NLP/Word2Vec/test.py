4f9d3edd3552d472ec69faaa76ef13af52d145e4
valentinewilson <valentinewilson@lawn-128-61-40-132.lawn.gatech.edu>
2016-10-25 18:22:13 -0400

    saving!

--- NLP/Word2Vec/test.py
+++ NLP/Word2Vec/test.py
@@ -1,8 +1,12 @@
 import logging
+import gensim
 from gensim.models import Word2Vec
 from gensim import corpora, models, similarities
 from nltk.corpus import brown
-b = Word2Vec(brown.sents())
-print(b.most_similar_cosmul(positive=['person','baby'],negative = ["dog"], topn=5))
+#b = Word2Vec(  brown.sents(), workers = 4)
+#b.save('/tmp/mymodel')
+
+new_model = gensim.models.Word2Vec.load('/tmp/mymodel')
+print(new_model.most_similar(positive=['person','baby','child'],negative = ['dog','tree'], topn=5))
 #COCA corpus
-#save a mmodel
+#save a mmodels

25b703c8f0c070ffa0104b359fba569d9c261581
Valentine W. <valentinewilson@lawn-128-61-37-140.lawn.gatech.edu>
2016-11-04 09:52:12 -0400

    regex to take out irrelevant text,splitting questions and answers based on digit plus period, need to load into dictionary based on number

--- NLP/Word2Vec/test2.py
+++ NLP/Word2Vec/test2.py
@@ -1,6 +1,17 @@
 #Regex
-import re
-with  open("analogy.txt") as f:
+import re,io
+with  io.open("501analogies.txt", encoding='utf-8') as f:
+  
     text = f.read()
-    p = re.compile('\d+')
     
+    text = text.replace("\n"," ")
+    text = text.replace("501"," ")
+    text = text.replace("Word"," ")
+    text = text.replace("Analogy"," ")
+    text = text.replace("Questions"," ")
+    p = re.compile('\d+\s+')
+    text = re.sub(p, "", text)
+    qa = {}
+    s = "3. hello 4. hi 6. nah"
+    text = re.split('\d+\.',text)
+    print(text)

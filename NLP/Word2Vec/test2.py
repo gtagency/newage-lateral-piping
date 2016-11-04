b426836124930c8b569b2c3c9d7e9738f740d683
Valentine W. <valentinewilson@lawn-128-61-37-140.lawn.gatech.edu>
2016-11-04 11:52:35 -0400

    added comments

--- NLP/Word2Vec/test2.py
+++ NLP/Word2Vec/test2.py
@@ -1,32 +1,34 @@
 #Regex
 import re,io
+# python doesnt like the files encoding
 with  io.open("501analogies.txt", encoding='utf-8') as f:
-  
     text = f.read()
-    
+    # formatting text
     text = text.replace("\n"," ")
     text = text.replace("501"," ")
     text = text.replace("Word"," ")
     text = text.replace("Analogy"," ")
     text = text.replace("Questions"," ")
+    #creating a regex for non relevent numbers (ones without a .)
     p = re.compile('\d+\s+')
+    # taking out all non relevent numbers
     text = re.sub(p, "", text)
     qa = {}
-    s = "3. hello 4. hi 6. nah 11. hooo 3. a"
+    #splitting on question/answer number
     b = re.split('(\d+\.)',text)
-    
+    #goes through the split string
     for i in range(len(b)):
+        # if there is a relevent number (question or answer number)
         if re.match("\d+", b[i]):
+            #get the number found
             num = re.findall("\d+", b[i])[0]  
+            #get the question or answers text
             txt = b[i+1]
+            # if the question is in the dictionary, append the answer to the question
             if(num in qa.keys()):
                 qa[num] = qa[num] + txt
             else:
+                #otherwise key = num, value = text
                 qa [num] = txt
-                #print(num)
-                #print(txt)
+    #test       
     print(qa["54"])
-           
-
-            
-   # print(text)

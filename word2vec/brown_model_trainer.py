11b8f51e6cfb13e19c022a05dd847f1039def73b
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2016-12-03 15:37:22 -0500

    Finish word2vec project

--- word2vec/brown_model_trainer.py
+++
@@ -1,4 +0,0 @@
-from nltk.corpus import brown
-from gensim.models import Word2Vec
-
-Word2Vec(brown.sents()).save('trained_brown')
---
+++ word2vec/src/train_four_corpuses.py
@@ -0,0 +1,4 @@
+from nltk.corpus import brown, reuters, europarl_raw, gutenberg
+from gensim.models import Word2Vec
+
+Word2Vec(brown.sents() + reuters.sents() + europarl_raw.english.sents() + gutenberg.sents()).save('../model/trained_four.dat')
